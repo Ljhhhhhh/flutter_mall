e10f218a32bf511a404d6593f9dc6364e16b44c4
lujiehui <lujiehui@woouo.com>
2020-01-14 18:00:42 +0800

    '个人资料'

--- lib/userInfo.dart
+++ lib/userInfo.dart
@@ -8,6 +8,7 @@ class UserinfoWidget extends StatefulWidget {
 }
 
 class UserinfoWidgetState extends State<UserinfoWidget> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,18 +27,71 @@ class UserinfoWidgetState extends State<UserinfoWidget> {
                     fontSize: 15, height: 21 / 15, color: Color(0xFF999999))),
           ),
           Container(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            color: Colors.white,
-            child: Column(
-              children: <Widget>[TextField()],
-            ),
-          ),
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              color: Colors.white,
+              // child: Column(
+              //   children: <Widget>[TextField()],
+              // ),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    // Container(
+                    //   decoration: new BoxDecoration(
+                    //     border: new Border(
+                    //         bottom:
+                    //             BorderSide(color: Colors.black54, width: 2.0)),
+                    //   ),
+                    //   child: TextFormField(
+                    //     decoration: const InputDecoration(
+                    //         hintText: '请输入手机号码',
+                    //         prefixText: '手机号码 ',
+                    //         prefixStyle: TextStyle(color: Colors.red)),
+                    //     validator: (value) {
+                    //       if (value.isEmpty) {
+                    //         return '请输入手机号码';
+                    //       }
+                    //       return null;
+                    //     },
+                    //   ),
+                    // ),
+                    // https://github.com/fluttercandies/wechat_flutter/blob/master/lib/ui/view/edit_view.dart
+                    TextFormField(
+                      decoration: new InputDecoration(
+                        prefixIcon: Container(
+                          alignment: Alignment.centerLeft,
+                          // mainAxisAlignment: MainAxisAlignment.center,
+                          child: Text('手机号码 ', style: TextStyle(fontSize: 16, height: 1),)
+                        ),
+                    ),),
+                    TextFormField(
+                      decoration: new InputDecoration(
+                        prefix: Text('姓名 '),
+                        focusColor: Colors.red
+                    ),),
+                    
+                    // TextFormField(
+                    //   decoration: const InputDecoration(
+                    //     hintText: '请输入姓名',
+                    //     prefixText: '姓名 ',
+                    //   ),
+                    //   validator: (value) {
+                    //     if (value.isEmpty) {
+                    //       return '请输入姓名';
+                    //     }
+                    //     return null;
+                    //   },
+                    // )
+                  ],
+                ),
+              )),
           Container(height: 60),
           Material(
               borderRadius: BorderRadius.circular(25),
               color: Color(0xFFF3C774), // 渐变背景
               child: MaterialButton(
-                child: Text("normal",
+                child: Text("确认",
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 18,
